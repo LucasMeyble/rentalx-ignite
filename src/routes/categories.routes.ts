872667ba878d1dade8b2c9080eb289f4a28680db
Lucas Meyble <lucasmeyble123@gmail.com>
2022-01-05 10:33:21 -0300

    inserindo tipagem para categoria

--- src/routes/categories.routes.ts
+++ src/routes/categories.routes.ts
@@ -1,18 +1,21 @@
 import { Router } from 'express';
-import { v4 as uuidV4 } from 'uuid';
+import { Category } from '../model/Category';
 
 const categoriesRoutes = Router();
 
-const categories = [];
+const categories: Category[] = [];
 
 categoriesRoutes.post('/', (req, res) => {
   const { name, description } = req.body;
 
-  const category = {
-    id: uuidV4(),
+  const category = new Category(); 
+  
+  //copiar os valores de todas as propriedades próprias enumeráveis de um ou mais objetos de origem para um objeto destino
+  Object.assign(category, {
     name,
     description,
-  }
+    created_at: new Date(),
+  });
 
   categories.push(category);
 
