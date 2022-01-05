13933001bff6f8d3b78f2eda95230eb48dc67ee8
Lucas Meyble <lucasmeyble123@gmail.com>
2022-01-05 11:41:56 -0300

    criando repositorio de categoria

---
+++ src/repositories/CategoriesRepository.ts
@@ -0,0 +1,31 @@
+import { Category } from '../model/Category';
+
+interface ICrateCategoryDTO {
+  name: string;
+  description: string
+}
+
+class CategoriesRepository{
+
+  private categories: Category[];
+
+  constructor(){
+    this.categories = [];
+  }
+
+  create({ name, description }: ICrateCategoryDTO): void{
+    const category = new Category(); 
+  
+    //copiar os valores de todas as propriedades próprias enumeráveis de um ou mais objetos de origem para um objeto destino
+    Object.assign(category, {
+      name,
+      description,
+      created_at: new Date(),
+    });
+
+    this.categories.push(category);
+  }
+
+}
+
+export { CategoriesRepository }
--- src/routes/categories.routes.ts
+++ src/routes/categories.routes.ts
@@ -1,25 +1,18 @@
 import { Router } from 'express';
-import { Category } from '../model/Category';
+import { CategoriesRepository } from '../repositories/CategoriesRepository';
 
 const categoriesRoutes = Router();
-
-const categories: Category[] = [];
+const categoriesRepository = new CategoriesRepository();
 
 categoriesRoutes.post('/', (req, res) => {
   const { name, description } = req.body;
 
-  const category = new Category(); 
-  
-  //copiar os valores de todas as propriedades próprias enumeráveis de um ou mais objetos de origem para um objeto destino
-  Object.assign(category, {
-    name,
-    description,
-    created_at: new Date(),
+  categoriesRepository.create({
+    name, 
+    description
   });
 
-  categories.push(category);
-
-  return res.status(201).json(categories).send();
+  return res.status(201).send();
 });
 
 export { categoriesRoutes };
