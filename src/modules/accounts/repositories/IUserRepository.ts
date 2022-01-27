91aae4b94b278e7222e3e73a5863ad356db24aac
lucas Meyble <lucasmeyble123@gmail.com>
2022-01-27 15:54:54 -0300

    criando repositorio de usuario

---
+++ src/modules/accounts/repositories/IUserRepository.ts
@@ -0,0 +1,10 @@
+import { ICreateUserDTO } from "../dtos/ICreateUserDTO";
+
+
+interface IUserRepository {
+
+  create(data: ICreateUserDTO): Promise<void>;
+
+}
+
+export { IUserRepository }
--- src/shared/container/index.ts
+++ src/shared/container/index.ts
@@ -1,9 +1,13 @@
 import { container } from 'tsyringe';
+
 import { ICategoriesRepository } from '../../modules/cars/repositories/ICategoriesRepository';
 import { CategoriesRepository } from '../../modules/cars/repositories/implementations/CategoriesRepository';
 
 import { ISpecificationsRepository } from '../../modules/cars/repositories/ISpecificationsRepository';
 import { SpecificationsRepository } from '../../modules/cars/repositories/implementations/SpecificationsRepository';
+
+import { IUserRepository } from '../../modules/accounts/repositories/IUserRepository';
+import { UsersRepository } from '../../modules/accounts/repositories/implementations/UsersRepository';
 
 container.registerSingleton<ICategoriesRepository>(
   "CategoriesRepository",
@@ -13,4 +17,9 @@ container.registerSingleton<ICategoriesRepository>(
 container.registerSingleton<ISpecificationsRepository>(
   "SpecificationsRepository",
   SpecificationsRepository
+);
+
+container.registerSingleton<IUserRepository>(
+  "UsersRepository",
+  UsersRepository
 )
