f112a3a1c0ec5d445d1796ba587a3ab97e5f77e0
Lucas Meyble <lucasmeyble123@gmail.com>
2022-01-28 12:01:27 -0300

    criptografando senha  verificando email existente

--- src/modules/accounts/useCases/createUser/CreateUserUseCase.ts
+++ src/modules/accounts/useCases/createUser/CreateUserUseCase.ts
@@ -1,4 +1,6 @@
 import { inject, injectable } from "tsyringe";
+import { hash } from "bcryptjs"
+
 import { ICreateUserDTO } from "../../dtos/ICreateUserDTO";
 import { IUserRepository } from "../../repositories/IUserRepository";
 
@@ -12,10 +14,18 @@ class CreateUserUseCase{
 
   async execute({ name, email, password, drive_license }: ICreateUserDTO): Promise<void>{
 
+    const userAreadyExists = await this.usersRepository.findByEmail(email);
+
+    if(userAreadyExists){
+      throw new Error("USer already exists");
+    }
+
+    const passwordHash = await hash(password, 8);
+
     await this.usersRepository.create({
       name, 
       email, 
-      password, 
+      password: passwordHash, 
       drive_license
     });
 
