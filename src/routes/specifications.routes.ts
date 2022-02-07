b9d283f0e683b456528097fdddfd5e3734b1a9bd
Lucas Meyble <lucasmeyble123@gmail.com>
2022-02-07 12:06:53 -0300

    feat(creeate specification): autenticação nas rotas

--- src/routes/specifications.routes.ts
+++ src/routes/specifications.routes.ts
@@ -1,10 +1,12 @@
 import { Router } from "express";
+import { ensureAuthenticated } from "../middlewares/ensureAuthenticated";
 import { CreateSpacificationController } from "../modules/cars/services/useCases/createSpecification/CreateSpecificationController";
 
 const specificationRoutes = Router();
 
 const createSpecificationController = new CreateSpacificationController();
 
+specificationRoutes.use(ensureAuthenticated)
 specificationRoutes.post("/", createSpecificationController.handle);
 
 export { specificationRoutes }
