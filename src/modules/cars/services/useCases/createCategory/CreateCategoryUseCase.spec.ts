2b15a08786ceeb211e6c48d518fbc59b8d2409d8
Lucas Meyble <lucasmeyble123@gmail.com>
2022-02-09 12:15:14 -0300

    feat(test): criando primeiro teste

---
+++ src/modules/cars/services/useCases/createCategory/CreateCategoryUseCase.spec.ts
@@ -0,0 +1,18 @@
+
+
+describe("Criar categoria", () => {
+  it("Espero que 2 + 2 seja 4", () => {
+    const soma = 2 + 2;
+    const result = 4;
+
+    expect(soma).toBe(result);
+
+  });
+
+  it("Espero que seja 2 + 2 não seja 5", () => {
+    const soma = 2 + 2;
+    const result = 5;
+
+    expect(soma).not.toBe(result);
+  });
+}); 
